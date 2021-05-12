ea4627e9de35b112cdf3d2ef8d8a511a04a51b1c
unknown <sarwarsunjid@gmail.com>
2021-05-13 03:00:08 +0600

    Install React Router Dom, Provide Support Using React Router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
-
 //Home
 import Home from './pages/Home'
-
 //Header 
 import Header from './components/Header'
-
 //Footer
 import Footer from './components/Footer'
+import About from './pages/About'
+import Categories from './pages/Categories'
+import Listing from './pages/Listing'
+
+import {BrowserRouter, Switch,Route, Router } from 'react-router-dom'
+
+//React router support
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+      <Router> 
+
+        <Header />
+
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/listing' component={Listing} />
+            <Route path='/categories' component={Categories} />
+          </Switch>
+
+        {/* <Home /> */}
+
+        <Footer />
+
+      </Router>
+      
     </div>
   );
 }
