031631c00cb6a05f0cacc66ad4627604b1002252
unknown <sarwarsunjid@gmail.com>
2021-05-13 02:13:59 +0600

    Home, Component, Header Component, Footer Component Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+
+//Home
 import Home from './pages/Home'
+
+//Header 
+import Header from './components/Header'
+
+//Footer
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
+      <Footer />
     </div>
   );
 }
